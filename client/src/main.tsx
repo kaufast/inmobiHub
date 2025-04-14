947ebd2ea3058129adf1d8acf0dbcd007edceee2
“kaufast” <kaufast@gmail.com>
2025-04-14 19:59:55 +0200

    Fix: Add missing App.css import

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './styles/globals.css'
+import './App.css'
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 // Import i18n configuration
